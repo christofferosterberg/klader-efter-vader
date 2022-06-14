ca9df5b713232ad4f41e0af16cb1d159d6f65fea
Christoffer <chros928@student.liu.se>
2022-06-14 20:00:10 +0200

    can get weather data for cities and store in db

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+import pytz
 
 app = Flask(__name__, static_folder='client', static_url_path='/')
 
@@ -12,6 +13,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'U0A6DRhYvG3XXgzWCUEGvu5F9UuvVCAiSYwicGbKIFpktoSb5WSgf7Fkp_YbAXhQ'
 db = SQLAlchemy(app)
 
+timezone = pytz.timezone("Europe/Stockholm")
+
+host = "http://localhost:3000"
+
+
+# Classes in db
 class City(db.Model):
     id        = db.Column(db.Integer, primary_key=True)
     name      = db.Column(db.String(80), unique=True, nullable=False)
@@ -32,7 +39,7 @@ class Weather(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     windSpeed   = db.Column(db.Float, nullable=False)
 
-    city_id     = db.Column(db.Integer, ForeignKey('city.id'))
+    city_id   = db.Column(db.Integer, ForeignKey('city.id'))
 
     def __repr__(self):
         return '<Weather {}: {} {} {} {} {}>'.format(self.id, self.datetime, self.description, self.temperature, self.windSpeed, self.city_id)
@@ -43,16 +50,48 @@ class Weather(db.Model):
 
 
 
-# db.init_app(app)
 
-host = "http://localhost:3000"
+#Gets a datetime in string format "YYYY-MM-DD HH:MM:SS"
+def str_datetime(datetime_string):
+    if datetime_string is None:
+        return datetime_string
+    if isinstance(datetime_string, datetime):
+        return datetime_string
+    date_time = timezone.localize(datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S'))
+    return date_time
+
 
 def getWeatherData():
-    return requests.get('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12/lat/58/data.json').json()
+
+    cities = City.query.all()
+    print(cities)
+
+    for city in cities:
+        lon = int(round(city.longitude, 0))
+        lat = int(round(city.latitude, 0))
+        data = requests.get('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/'+str(lon)+'/lat/'+str(lat)+'/data.json').json()
+        timeSeries = data['timeSeries']
+    
+        for timeSerie in timeSeries:
+            date = timeSerie['validTime'][0:10]
+            time = timeSerie['validTime'][11:19]
+            dt = str(date) + ' ' + str(time)
+
+            parameters  = timeSerie['parameters']
+            description = parameters[18]['values'][0]
+            temperature = parameters[1]['values'][0]
+            windSpeed   = parameters[4]['values'][0]
+
+            newWeather = Weather(datetime=str_datetime(dt), description=description, temperature=temperature, windSpeed=windSpeed, city_id=city.id)
+            db.session.add(newWeather)
+    
+    db.session.commit()
+
+
 
 @app.route("/")
 def client():
-    print(getWeatherData())
+    getWeatherData()
     return app.send_static_file("client.html")
 
 if __name__ == '__main__':
--- fill_db.py
+++ fill_db.py
@@ -3,6 +3,6 @@ from app import db, City
 db.drop_all()
 db.create_all()
 
-gothenburg = City(name='Göteborg', longitude=57.708870, latitude=11.974560)
+gothenburg = City(name='Göteborg', longitude=11.974560, latitude=57.708870)
 db.session.add(gothenburg)
 db.session.commit()
