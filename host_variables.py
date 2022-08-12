bb24686dcab4a375fe8eb7a5c6d981db74de0678
Christoffer <chros928@student.liu.se>
2022-08-12 11:56:44 +0200

    changed variables

--- host_variables.py
+++ host_variables.py
@@ -1,1 +1,1 @@
-DATABASE_URL = 'sqlite:///database.db'
+DATABASE_URL = 'postgresql://xrpoqreqhsiilo:de4af35557999658292080d1bbb07fe66911689d266ac5e65a0cb2bcbef9aded@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/dealfu4qjm0mc3'
